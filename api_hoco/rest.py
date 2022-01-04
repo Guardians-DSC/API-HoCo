eb6205da90bc100bc7732c8712acaeb5c79c7dae
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-04 11:23:31 -0300

    Migra rotas para diretório separado

--- api_hoco/rest.py
+++ api_hoco/rest.py
@@ -1,15 +1,6 @@
 from flask import Flask
-from flask import request
-from flask import jsonify
+from api_hoco.routes.api import api
 
 app = Flask(__name__)
 
-
-@app.route("/status", methods=['GET'])
-def status():
-    '''Retorna um simples json com status 200'''
-    status = {
-        "status": "operacional",
-        "service": "api-flask-example",
-    }
-    return jsonify(status), 200
+app.register_blueprint(api)
---
+++ api_hoco/routes/api.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+from flask import jsonify
+from datetime import datetime
+
+api = Blueprint('api', __name__, template_folder='templates')
+
+@api.route("/status", methods=['GET'])
+def status():
+    '''Retorna um simples json com status 200'''
+    current_timestamp = datetime.now()
+
+    status = {
+        "status": "operacional",
+        "service": "api-flask-example",
+        "timestamp": datetime.timestamp(current_timestamp),
+        "date": current_timestamp,
+    }
+    return jsonify(status), 200
+
