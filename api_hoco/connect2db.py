46f92740188c173dd6398c838e46ead602b0e3f3
dnlgomesl <daniel.gomes.lima@ccc.ufcg.edu.br>
2023-01-30 23:19:14 -0300

    add uri bd

--- api_hoco/connect2db.py
+++ api_hoco/connect2db.py
@@ -1,6 +1,6 @@
 import pymongo
 
-API_HOCO_DB_URI = "mongodb://bd-mongo:27017/hoco"
+API_HOCO_DB_URI = "mongodb+srv://hoco:hocop1@hoco-db.6oqk6tj.mongodb.net/hoco"
 
 MONGO_CLIENT = pymongo.MongoClient(API_HOCO_DB_URI)
 
